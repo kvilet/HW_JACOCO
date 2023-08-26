153a9066a4f0151d1a0860ad58836879c7e9c282
Janna <jannasheyanova@gmail.com>
2023-08-26 23:42:29 +0300

    Added Tests

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -15,4 +15,26 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMaxIfMaxZero() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {-12, -5, -8, -4, -5, -3, -8, -6, 0, -11, -12};
+        long expected = 0;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+    @Test
+    void findMaxIfAllEqual() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1};
+        long expected = 1;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
